2c1db7cb4b4aaf4031ec4d38e52299dd614dd95e
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-11-14 19:02:28 +0200

    fix bugs

--- src/api-servise.js
+++ src/api-servise.js
@@ -1,5 +1,4 @@
-function fetchCountries(name) {
-    fetch(`https://restcountries.com/v2/${name}?fields=name.official,capital,population,flags.svg,languages`).then(data => data.json());
-}
+export default function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`).then(data => data.json());
+};
 
-export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import {fetchCountries} from './api-servise'
+import fetchCountries from './api-servise'
 import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
@@ -12,10 +12,19 @@ refs.input.addEventListener('input',debounce(inInput, DEBOUNCE_DELAY) );
 
 function inInput(e) {
     let name = e.target.value;
-    console.log(name);
-    fetchCountries(name);
+    console.log(Boolean(name));
+    if (name) {
+        fetchCountries(name).then((data) => {
+            const countries = data.map((countrie) => {
+                countrie.flag
+            })
+            console.log(countries)
+
+        } );
+    }
+
 }
 
 
 // fetchCountries(Ukraine);
-// https://restcountries.com/v2/all?fields=name,capital,currencies
+// https://restcountries.com/v2/all
