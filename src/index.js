b2065e3bba454da9fd4c06cf76b124f0a463e360
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-11-14 23:20:02 +0200

    Finally fix

--- src/index.js
+++ src/index.js
@@ -5,24 +5,58 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 const refs = {
     input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    info: document.querySelector('.country-info'),
 }
-
+ console.log(refs.list)
 
 refs.input.addEventListener('input',debounce(inInput, DEBOUNCE_DELAY) );
 
 function inInput(e) {
-    let name = e.target.value;
+    let name = e.target.value.trim();
     console.log(Boolean(name));
-    if (name) {
-        fetchCountries(name).then((data) => {
-            const countries = data.map((countrie) => {
-                countrie.flag
-            })
-            console.log(countries)
+   if (name) {
+       fetchCountries(name).then(createMarkup).catch(eroro => {
+           console.log(eroro)
+           refs.info.innerHTML = ' ';
+           refs.list.innerHTML = ' ';
+    });
+    
+   } else {
+       refs.list.innerHTML = ' ';
+       
+   }
 
-        } );
+}
+
+function createMarkup(data) {
+    console.log(data.length)
+    if (data.length === 1) {
+     console.log(data)
+        const markupCountrieInfo = data.map((coutrie) => `
+        <li class = "country-list__item">
+            <img class = "country-list__img" src = "${coutrie.flags.svg}" width="50" >
+            <p class = "country-list__article">${coutrie.name}</p>
+        </li>
+        <p>Capital:${coutrie.capital}</p>
+        <p>Population:${coutrie.population}</p>
+        <p>Capital:${coutrie.languages.name}</p>
+        `).join('');
+        refs.list.innerHTML = ' ';
+        refs.info.innerHTML = markupCountrieInfo;
+    } else {
+        const markupList = data.map((coutrie) => `
+        <li class = "country-list__item">
+            <img class = "country-list__img" src = "${coutrie.flags.svg}" width="50" >
+            <p class = "country-list__article">${coutrie.name}</p>
+        </li>
+        `).join('');
+        
+        refs.list.innerHTML = markupList;
+        refs.info.innerHTML = ' ';
+        
     }
-
+    
 }
 
 
